dfeebebf8d6c87b1eca0d2c115d4bdfb3842c2a7
Francesco <francesco.donini@iit.cnr.it>
2019-11-10 02:06:01 +0100

    fixed _id in id in USerDTo

--- src/main/java/com/unicam/chorchain/user/UserDTO.java
+++ src/main/java/com/unicam/chorchain/user/UserDTO.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 
 @Data
 public class UserDTO {
-    private String _id;
+    private String id;
     private String address;
     private LocalDateTime created;
 //    private String password;
