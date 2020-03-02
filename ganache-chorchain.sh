ee2b9808c4734e13e48501012a4433931f44e59b
Francesco <francesco.donini@iit.cnr.it>
2020-03-02 09:38:25 +0100

    added unlimitedSize flag

--- ganache-chorchain.sh
+++ ganache-chorchain.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 
-ganache-cli  -d --db ganache-db --port 8545 -l 900000000 -g 20000000000 --accounts 10 --mnemonic 'include poem goose genuine baby flat mom token drama harsh sadness fit' --networkId 5777 --verbose
+ganache-cli  -d --db ganache-db --port 8545 -l 9000000000 -g 20000000000 --accounts 10 --mnemonic 'include poem goose genuine baby flat mom token drama harsh sadness fit' --networkId 5777 --verbose --allowUnlimitedContractSize
