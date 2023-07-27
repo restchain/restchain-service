ae9a33cd7ee9a9bf797ae9c1db252a70b6f1a2c7
Francesco <francesco.donini@iit.cnr.it>
2023-07-27 11:33:07 +0200

    added cors directives

--- src/main/java/com/unicam/chorchain/ChorchainServiceApplication.java
+++ src/main/java/com/unicam/chorchain/ChorchainServiceApplication.java
@@ -2,11 +2,24 @@ package com.unicam.chorchain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ChorchainServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ChorchainServiceApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost").allowedOrigins("http://localhost:81").allowedOrigins("http://localhost:82").allowedOrigins("http://localhost:83");
+            }
+        };
     }
 }
